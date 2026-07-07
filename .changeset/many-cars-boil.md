8d76227527278b81dcf9931404a1606a90c48e7f
Jens Simon <jens.simon80@googlemail.com>
2026-07-07 13:18:33 +0200

    feat: new rule ignores for eslint-plugin-unicorn

---
+++ .changeset/many-cars-boil.md
@@ -0,0 +1,11 @@
+---
+"@commerce-klaus/eslint-config-sfcc": minor
+---
+
+Added new rule ignores for eslint-plugin-unicorn
+
+- `unicorn/logical-assignment-operators`
+- `unicorn/no-computed-property-existence-check`
+- `unicorn/prefer-iterator-to-array-at-end`
+- `unicorn/prefer-logical-operator-over-ternary`
+- `unicorn/prefer-unicode-code-point-escapes`
--- packages/eslint-config-sfcc/src/rules/unicorn.ts
+++ packages/eslint-config-sfcc/src/rules/unicorn.ts
@@ -3,8 +3,11 @@ import type { Linter } from "eslint"
 // Disable eslint-plugin-unicorn rules that suggest or require ES2015+ syntax
 // not supported on SFCC sandboxes (Rhino engine).
 const unicorn: Linter.RulesRecord = {
+  "unicorn/logical-assignment-operators": "off",
+
   "unicorn/no-array-for-each": "off",
   "unicorn/no-array-sort": "off",
+  "unicorn/no-computed-property-existence-check": "off",
   "unicorn/no-for-loop": "off",
   "unicorn/no-useless-iterator-to-array": "off",
 
@@ -14,6 +17,8 @@ const unicorn: Linter.RulesRecord = {
   "unicorn/prefer-array-flat-map": "off",
   "unicorn/prefer-at": "off",
   "unicorn/prefer-default-parameters": "off",
+  "unicorn/prefer-iterator-to-array-at-end": "off",
+  "unicorn/prefer-logical-operator-over-ternary": "off",
   "unicorn/prefer-modern-math-apis": "off",
   "unicorn/prefer-module": "off",
   "unicorn/prefer-node-protocol": "off",
@@ -22,6 +27,7 @@ const unicorn: Linter.RulesRecord = {
   "unicorn/prefer-structured-clone": "off",
   "unicorn/prefer-string-replace-all": "off",
   "unicorn/prefer-spread": "off",
+  "unicorn/prefer-unicode-code-point-escapes": "off",
 }
 
 export default unicorn
